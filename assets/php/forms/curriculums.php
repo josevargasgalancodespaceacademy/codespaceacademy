1184be72ad432a62b3896509d897b0e42505ebf4
davidfisher24 <davidfisher24@gmail.com>
2017-09-14 16:40:44 +0200

    Validation messages for file upload functions

--- assets/php/forms/curriculums.php
+++ assets/php/forms/curriculums.php
@@ -8,22 +8,18 @@ require_once '../config.php';
 $request = $_POST;
 
 if(isset($_FILES['curriculum'])){
-	$file_upload_errors= array();
+	$file_upload_errors = array();
 	$file_name = $_FILES['curriculum']['name']; 
 	$file_size = $_FILES['curriculum']['size']; 
 	$file_tmp = $_FILES['curriculum']['tmp_name']; 
 	$file_type = $_FILES['curriculum']['type']; 
 
+
 	if($file_type !== 'application/pdf'){
 		$file_upload_errors[] = "La extension no esta permitidio. Por favor, escoge un pdf.";
-	}
-
-	if($file_size > 5242880){
+	} else if ($file_size > 5242880){
 		$file_upload_errors[] = 'Tamaño tiene que ser menos de 5MB';
-	}
-
-
-	if(empty($file_upload_errors) == true){
+	} else if(empty($file_upload_errors) == true){
 		$folder = "../../../curriculums/";
 		$time = date("Y-m-d");
 		$saved_file = $folder . $time . "-" . $file_name;
@@ -43,7 +39,8 @@ $validator->filledIn("name")->alpha("name")->length("name", "<=", 100);
 $validator->filledIn("email")->length("email", "<=", 100)->email("email");
 $validator->filledIn("telephone")->numeric("telephone",array(" ","+"))->length("telephone", "<=", 15);
 
-$errors = array_merge($validator->getErrors(),$file_upload_errors);
+$errors = $validator->getErrors();
+foreach ($file_upload_errors as $file_upload_error) $errors["file_upload"] = $file_upload_error;
 
 if (!$errors) {
 	$mysql = new Mysql(DB_SERVER,DB_USER,DB_PASSWORD,DB_NAME);
