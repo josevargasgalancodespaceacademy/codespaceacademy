b38bdcb043a0170d5882203590b75521fe539759
josevargasgalancodespaceacademy <jose.f.vargas@codespaceacademy.com>
2017-09-18 18:28:37 +0200

    politica de cookies

--- assets/javascript/codespace.js
+++ assets/javascript/codespace.js
@@ -160,39 +160,39 @@ function datepicker() {
   };
 
    /*Cookies*/
-/*function getCookie(c_name){
-    var c_value = document.cookie;
-    var c_start = c_value.indexOf(" " + c_name + "=");
-    if (c_start == -1){
-        c_start = c_value.indexOf(c_name + "=");
+function getCookie(c_name){
+  var c_value = document.cookie;
+  var c_start = c_value.indexOf(" " + c_name + "=");
+  if (c_start == -1){
+    c_start = c_value.indexOf(c_name + "=");
+  }
+  if (c_start == -1){
+    c_value = null;
+  }else{
+    c_start = c_value.indexOf("=", c_start) + 1;
+    var c_end = c_value.indexOf(";", c_start);
+    if (c_end == -1){
+      c_end = c_value.length;
     }
-    if (c_start == -1){
-        c_value = null;
-    }else{
-        c_start = c_value.indexOf("=", c_start) + 1;
-        var c_end = c_value.indexOf(";", c_start);
-        if (c_end == -1){
-            c_end = c_value.length;
-        }
-        c_value = unescape(c_value.substring(c_start,c_end));
-    }
-    return c_value;
+    c_value = unescape(c_value.substring(c_start,c_end));
+  }
+  return c_value;
 }
- 
+
 function setCookie(c_name,value,exdays){
-    var exdate=new Date();
-    exdate.setDate(exdate.getDate() + exdays);
-    var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-    document.cookie=c_name + "=" + c_value;
-}
- if(getCookie('tiendaaviso')!="1"){
-    document.getElementById("barraaceptacion").css('display','block');
+  var exdate=new Date();
+  exdate.setDate(exdate.getDate() + exdays);
+  var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+  document.cookie=c_name + "=" + c_value;
 }
 
+if(getCookie('codespaceaviso')!="1"){
+  $("#barraaceptacion").css('display', 'block');
+}
 function PonerCookie(){
-    setCookie('tiendaaviso','1',365);
-    document.getElementById("barraaceptacion").css('display','none');
-
+  setCookie('codespaceaviso','1',365);
+  document.getElementById("barraaceptacion").style.display = "none";
+}
 
 /**
  * Sets a focus event for inputs with errors. On focus the error message
