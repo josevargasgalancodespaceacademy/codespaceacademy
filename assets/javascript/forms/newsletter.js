72bad0976d77f97d95abe04c2f54c6306638e04e
José Francisco Vargas Galán <31619519+josevargasgalancodespaceacademy@users.noreply.github.com>
2017-09-28 11:36:16 +0200

    Update newsletter.js

--- assets/javascript/forms/newsletter.js
+++ assets/javascript/forms/newsletter.js
@@ -26,7 +26,14 @@ if(respuesta == "OK")
           $('.newsletter-email').val("");
           $('.newsletter-email').attr("placeholder", data['email']);
           replaceValueTimeout('.newsletter-email',originalData['email']);
-        }
+    }
+      if (data.hasOwnProperty('general'))
+    {
+          $('.newsletter-email').addClass("errorbox");
+          $('.newsletter-email').val("");
+          $('.newsletter-email').attr("placeholder", data['general']);
+          replaceValueTimeout('.newsletter-email',originalData['general']);
+    }
       else 
        {
         $('.newsletter-email').off('focus');
