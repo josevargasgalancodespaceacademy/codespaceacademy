1cd9a4f7a24ce7011eba0c777203fb763df31a0c
josevargasgalancodespaceacademy <jose.f.vargas@codespaceacademy.com>
2017-10-30 14:16:46 +0100

    mejoras backend

---
+++ admin/analitics/index.php
@@ -0,0 +1,36 @@
+<?php
+/*header("HTTP/1.1 301 Moved Permanently");
+header("Location: https://www.codespaceacademy.com");*/
+function getRealIP()
+{
+
+    if (isset($_SERVER["HTTP_CLIENT_IP"]))
+    {
+        return $_SERVER["HTTP_CLIENT_IP"];
+    }
+    elseif (isset($_SERVER["HTTP_X_FORWARDED_FOR"]))
+    {
+        return $_SERVER["HTTP_X_FORWARDED_FOR"];
+    }
+    elseif (isset($_SERVER["HTTP_X_FORWARDED"]))
+    {
+        return $_SERVER["HTTP_X_FORWARDED"];
+    }
+    elseif (isset($_SERVER["HTTP_FORWARDED_FOR"]))
+    {
+        return $_SERVER["HTTP_FORWARDED_FOR"];
+    }
+    elseif (isset($_SERVER["HTTP_FORWARDED"]))
+    {
+        return $_SERVER["HTTP_FORWARDED"];
+    }
+    else
+    {
+        return $_SERVER["REMOTE_ADDR"];
+    }
+
+}
+
+echo $_SERVER['HTTP_USER_AGENT']. "<br>";
+echo getRealIP();
+?>
