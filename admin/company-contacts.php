b7c349a2db41882bd6877757b090ea0b7dd85a82
josevargasgalancodespaceacademy <jose.f.vargas@codespaceacademy.com>
2017-10-31 13:55:43 +0100

    admin web

--- admin/company-contacts.php
+++ admin/company-contacts.php
@@ -47,11 +47,11 @@
       <li class="menu-item">
         <a href="newsletter">Newsletter</a>
       </li>
-      <li>
-            <input type="button" id="exportarexcel" value="Descargar Excel">
-      </li>
     </ul>
     </nav>
+      <li style="list-style: none">
+       <input type="button" id="exportarexcel" value="Descargar Excel">
+      </li>
     </div>
     </div>
     <div id="datos">
