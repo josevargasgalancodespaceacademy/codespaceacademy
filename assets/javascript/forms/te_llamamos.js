14ca47d1c6509ad4913025979b48ac78c396c55c
josevargasgalancodespaceacademy <jose.f.vargas@codespaceacademy.com>
2017-10-31 13:02:33 +0100

    formulario llamamos

---
+++ assets/javascript/forms/te_llamamos.js
@@ -0,0 +1,44 @@
+function enviar_llamamos_form(){
+          var url ="assets/php/forms/te_llamamos.php";
+  if ($('#check-llamamos').is(':checked')){ 
+  $.ajax({                        
+    type: "POST",                 
+    url: url,  
+    data: $("#formulario-llamamos").serialize()                   
+  }).done(function(respuesta){
+    var postData = $("#formulario-llamamos").serializeArray().reduce(function(obj, item) {
+    obj[item.name] = item.name;
+    return obj;
+    }, {});
+    var originalData = $("#formulario-llamamos").serializeArray().reduce(function(obj, item) {
+      obj[item.name] = item.value;
+      return obj;
+    }, {});
+    console.log(respuesta);
+    if( respuesta == "\nOK") {
+      $('#modal-te-llamamos').modal('show');
+    } else {
+      var data = JSON.parse(respuesta);
+      $('#formulario-llamamos .mensajerror').css('display', 'inline-block');
+      for (var key in postData) {
+        for (var key2 in data){
+          if (postData[key] == key2) {
+            $('#'+ key).addClass("errorbox");
+            $('#' + key).attr("placeholder", data[key]);  
+            $('#' + key).val("");
+            replaceValueTimeout('#'+ key,originalData[key]);
+            break
+          } else {  
+            $('#'+ key).off('focus');
+            $('#'+ key).removeClass("errorbox");
+            $('#' + key).attr("placeholder", ""); 
+          }
+        }
+      }
+    }
+  })
+}else
+  {
+    alert ('Acepta las condiciones legales');
+  }
+}
