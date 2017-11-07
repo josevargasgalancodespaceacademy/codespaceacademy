730405b6733b91f06381978080c4380c94c5ca15
josevargasgalancodespaceacademy <jose.f.vargas@codespaceacademy.com>
2017-11-07 18:56:11 +0100

    mejoras web

---
+++ assets/javascript/forms/taller.js
@@ -0,0 +1,43 @@
+function enviar_taller_form(){
+  var url = "../assets/php/forms/taller.php";
+  if ($('#check-taller').is(':checked')){ 
+  $.ajax({                        
+    type: "POST",                 
+    url: url,  
+    data: $("#formulario-taller").serialize()                   
+  }).done(function(respuesta){
+    var postData = $("#formulario-taller").serializeArray().reduce(function(obj, item) {
+    obj[item.name] = item.name;
+    return obj;
+    }, {});
+    var originalData = $("#formulario-taller").serializeArray().reduce(function(obj, item) {
+      obj[item.name] = item.value;
+      return obj;
+    }, {});
+    if( respuesta == "\nOK") {
+      $('#modal-taller').modal('show');
+    } else {
+      var data = JSON.parse(respuesta);
+      $('#formulario-taller .mensajerror').css('display', 'inline-block');
+      for (var key in postData) {
+        for (var key2 in data){
+          if (postData[key] == key2) {
+            $('#'+ key).addClass("errorbox");
+            $('#' + key).attr("placeholder", data[key]);  
+            $('#' + key).val("");
+            replaceValueTimeout('#'+ key,originalData[key]);
+            break
+          } else {  
+            $('#'+ key).off('focus');
+            $('#'+ key).removeClass("errorbox");
+            $('#' + key).attr("placeholder", ""); 
+          }
+        }
+      }
+    }
+  })
+}else
+  {
+    alert ('Acepta las condiciones legales');
+  }
+}
