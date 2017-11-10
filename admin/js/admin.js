78f0746b60d7d47c2affa32d8334631f0f69fc34
josevargasgalancodespaceacademy <jose.f.vargas@codespaceacademy.com>
2017-11-10 10:55:22 +0100

    mejoras admin

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
      "pagingType": "full_numbers",
        "language": {
     "lengthMenu": 'Muestra <select>'+
-      '<option value="5">5</option>'+
       '<option value="10">10</option>'+
       '<option value="15">15</option>'+
+      '<option value="20">20</option>'+
       '</select> filas'
   }
     } );
