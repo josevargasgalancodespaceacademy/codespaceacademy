60769f8d7a59a63dfa375920ea902f0525eba624
josevargasgalancodespaceacademy <jose.f.vargas@codespaceacademy.com>
2017-11-10 11:33:35 +0100

    mejoras admin

--- admin/te-llamamos.php
+++ admin/te-llamamos.php
@@ -68,7 +68,7 @@
   </thead>
  <?php
                 foreach ($company as $key => $company_contacts) {
-                  echo "<tr><td>".$company[$key]["name"]."</td><td>".$company[$key]["telephone"]."</td><td>".$company[$key]["created_at"]."</td></tr>";
+                  echo "<tr><td>".$company[$key]["name"]."</td><td>".$company[$key]["telephone"]."</td><td>".date("d-m-Y H:i:s", strtotime($company[$key]["created_at"]))."</td></tr>";
                 }
                ?>        
   </table>
