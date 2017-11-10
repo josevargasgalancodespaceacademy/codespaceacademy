a20104554cd11659a6980677f617d04eaf6cb227
josevargasgalancodespaceacademy <jose.f.vargas@codespaceacademy.com>
2017-11-10 11:14:35 +0100

    mejoras admin

--- admin/company-contacts.php
+++ admin/company-contacts.php
@@ -72,9 +72,10 @@
     </tr>
    </thead> 
  <?php
+ 
                 foreach ($company as $key => $company_contacts) {
-                   
-                  echo "<tr><td>".$company[$key]["name"]."</td><td>".$company[$key]["email"]."</td><td>".$company[$key]["telephone"]."</td><td>".$company[$key]["company_name"]."</td><td>".$company[$key]["company_link"]."</td><td>".$company[$key]["training_request"]."</td><td>".$company[$key]["comment"]."</td><td>".$company[$key]["created_at"]."</td></tr>";
+                  $date = date_create($company[$key]["created_at"]);
+                  echo "<tr><td>".$company[$key]["name"]."</td><td>".$company[$key]["email"]."</td><td>".$company[$key]["telephone"]."</td><td>".$company[$key]["company_name"]."</td><td>".$company[$key]["company_link"]."</td><td>".$company[$key]["training_request"]."</td><td>".$company[$key]["comment"]."</td><td>".date_format($date,'d-m-Y H:i:s');."</td></tr>";
                 }
                ?>           
   </table>  
