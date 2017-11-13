b2b19c8cfb39c9e5c30c25416d23f8f7da50c6c1
josevargasgalancodespaceacademy <jose.f.vargas@codespaceacademy.com>
2017-11-13 10:03:19 +0100

    mejoras admin

--- admin/index.php
+++ admin/index.php
@@ -57,7 +57,7 @@
       </li>
     </div>
     </div>
-    <div id="datos" class="col-xs-12">
+    <div id="datos" class="table-responsive">
   <table class="display" cellspacing="0" width="100%" id="tabla-datos">
     <thead>
   <tr>
