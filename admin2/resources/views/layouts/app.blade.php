8e8892583282dc8cde45e16f2b480a403a5b72ec
josevargasgalancodespaceacademy <jose.f.vargas@codespaceacademy.com>
2017-12-12 10:12:46 +0100

    mejoras admin

--- admin2/resources/views/layouts/app.blade.php
+++ admin2/resources/views/layouts/app.blade.php
@@ -45,7 +45,6 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
                         @else
                               <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
@@ -84,7 +83,7 @@
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
+                                            Cerrar sesión
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
