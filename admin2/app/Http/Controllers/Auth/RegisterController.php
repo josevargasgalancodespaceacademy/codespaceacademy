4a3378f4e85c9c1219503368847bdc3988fc6403
josevargasgalancodespaceacademy <jose.f.vargas@codespaceacademy.com>
2017-12-21 12:54:08 +0100

    mejoras admin

--- admin2/app/Http/Controllers/Auth/RegisterController.php
+++ admin2/app/Http/Controllers/Auth/RegisterController.php
@@ -49,6 +49,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
             'username' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
@@ -65,6 +66,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'last_name' => $data['last_name'],
             'username' => $data['username'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
