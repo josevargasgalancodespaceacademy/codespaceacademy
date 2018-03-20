cdca88b07d88477fe500b7223703fdacc77fa0bf
josevargasgalancodespaceacademy <jose.f.vargas@codespaceacademy.com>
2018-03-20 11:29:48 +0100

    curso scrum

---
+++ assets/php/forms/business_courses.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once '../classes/mysql.php'; 
+require_once '../classes/validator.php';
+require_once '../classes/sanitizer.php';
+require_once '../config.php';
+
+
+$request = $_POST;
+$sanitizer = new Sanitizer($request);
+$request = $sanitizer->sanitizeRequest();
+
+$validator = new Validator($request);
+$validator->filledIn("name")->alpha("name")->length("name", "<=", 100);
+$validator->filledIn("email")->length("email", "<=", 100)->email("email");
+$validator->filledIn("telephone")->numeric("telephone",array(" ","+"))->lengthBetween("telephone", 15, 9, $inclusive = true);
+$errors = $validator->getErrors();
+
+
+if (!$errors) {
+	$mysql = new Mysql(DB_SERVER,DB_USER,DB_PASSWORD,DB_NAME);
+	$mysql->insertRow("information_business_courses",$request);
+}
+
+if (!$errors) echo "OK";
+else echo json_encode($errors);
+
+
+?>
