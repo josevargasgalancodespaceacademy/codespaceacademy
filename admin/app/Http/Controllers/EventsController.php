65579af59ceb87b0f0efffef7a7dccb2850a63a8
José Francisco Vargas Galán <jose.f.vargas@codespaceacademy.com>
2018-04-12 10:21:48 +0200

    Automatizacion creacion eventos

--- admin/app/Http/Controllers/EventsController.php
+++ admin/app/Http/Controllers/EventsController.php
@@ -25,7 +25,7 @@ class EventsController extends Controller
          //$file = $request->file('event_image')->store('public');
          $file = $request->file('event_image')->storeAs('public', $request->file('event_image')->getClientOriginalName());
          $Events->event_image = $request->file('event_image')->getClientOriginalName();
-         @move_uploaded_file($origen, $destino);
+         //@move_uploaded_file($origen, $destino);
          //$Events->save();
          return redirect()->route('home');
 }
