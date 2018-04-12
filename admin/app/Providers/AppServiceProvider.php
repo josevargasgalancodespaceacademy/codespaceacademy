eaa683782644922bb8d3f44dbc78f91b2e49a9c8
José Francisco Vargas Galán <jose.f.vargas@codespaceacademy.com>
2018-04-12 11:31:40 +0200

    cambios admin

--- admin/app/Providers/AppServiceProvider.php
+++ admin/app/Providers/AppServiceProvider.php
@@ -24,8 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind('path.public', function() {
-            return base_path().'/../assets';
-        });
+        //
     }
 }
