78ee512e1268be1254aaf3efec186661b0ca5b14
José Francisco Vargas Galán <jose.f.vargas@codespaceacademy.com>
2018-04-12 10:04:36 +0200

    Automatizacion creacion eventos

--- admin/app/Http/Controllers/EventsController.php
+++ admin/app/Http/Controllers/EventsController.php
@@ -23,13 +23,14 @@ class EventsController extends Controller
          $Events->event_description =  $request->event_description;
          $Events->event_url =  $request->event_url;
          //$file = $request->file('event_image')->store('public');
-         $file = $request->file('event_image')->storeAs('public', $request->file('event_image')->getClientOriginalName());
-         $Events->event_image = $request->file('event_image')->hashName();
-         //$origen=$_FILES["archivo"]["tmp_name"];
-         //@move_uploaded_file($origen, $destino);
+         //$file = $request->file('event_image')->storeAs('public', $request->file('event_image')->getClientOriginalName());
+         $Events->event_image = $request->file('event_image')->getClientOriginalName();
+         $origen=$_FILES[$request->file('event_image')][$request->file('event_image')->getRealPath()];
+         $destino="https://codespaceacademy.com/assets/images/eventos/".$_FILES[$request->file('event_image')][$request->file('event_image')->getClientOriginalName()];
+         @move_uploaded_file($origen, $destino);
          //$Events->save();
          //return redirect()->route('home');
-         dd($request->file('event_image')->getRealPath());
+         dd($_FILES[$request->file('event_image')][$request->file('event_image')->getRealPath()]);
 }
      }
 }
