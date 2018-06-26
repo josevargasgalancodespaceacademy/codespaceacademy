3580b63bf4608f7ab9a5c31880aa2d88bbfaa57c
José Francisco Vargas Galán <jose.f.vargas@codespaceacademy.com>
2018-06-26 12:56:24 +0200

    slider partners

--- assets/javascript/codespace.js
+++ assets/javascript/codespace.js
@@ -119,34 +119,35 @@ function contador_becas() {
 
 
 /*animacion anclas usabilidad*/
-$(function () {
+if(window.location.href.indexOf('index')>-1){
+  $(function () {
 
-  $('a[href*="#"]:not([href="#"])').click(function () {
-
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      && location.hostname == this.hostname) {
-
-      var $target = $(this.hash);
-
-      $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
-
-      if ($target.length) {
-
-        var targetOffset = $target.offset().top;
-
-        $('html,body').animate({ scrollTop: targetOffset }, 1000);
-
-        return false;
-
+    $('a[href*="#"]:not([href="#"])').click(function () {
+  
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        && location.hostname == this.hostname) {
+  
+        var $target = $(this.hash);
+  
+        $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+  
+        if ($target.length) {
+  
+          var targetOffset = $target.offset().top;
+  
+          $('html,body').animate({ scrollTop: targetOffset }, 1000);
+  
+          return false;
+  
+        }
+  
       }
-
-    }
-
+  
+    });
+  
+  
   });
-
-
-});
-
+}
 /*Despliegue de menú móvil*/
 $(document).ready(function () {
   $('.hamburger-icon').click(function (e) {
