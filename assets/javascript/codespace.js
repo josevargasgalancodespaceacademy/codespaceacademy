94c29f553936a4075f9d7634e2f8085f4111d03e
José Francisco Vargas Galán <jose.f.vargas@codespaceacademy.com>
2018-09-07 10:13:39 +0200

    Cambio contador principal página index

--- assets/javascript/codespace.js
+++ assets/javascript/codespace.js
@@ -72,7 +72,7 @@ $(document).ready(function () {
 
 /*contador hacia atras del primer bootcamp*/
 function contador() {
-  var launchdate = new Date(2018, 9 - 1, 17);//la fecha de lanzamiento del bootcamp
+  var launchdate = new Date(2018, 9 - 1, 28);//la fecha de lanzamiento del bootcamp
   var format = 'DD hh:mm:ss';
   $('#counter').countdown({
 
